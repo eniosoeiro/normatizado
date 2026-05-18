4c14411d4ce51cf003e598aeedd0f1c961e4d2dc
Enio Costa Soeiro <eniosoeiro@Mac.localdomain>
2026-05-18 03:52:16 -0300

    fix: use vps.normatizando.com.br hostname to bypass CF Worker IP block

--- astro/functions/forum/[[all]].ts
+++ astro/functions/forum/[[all]].ts
@@ -8,18 +8,11 @@ export async function onRequest(
   const { request, env } = context;
   const origin = env.VPS_DISCOURSE_ORIGIN;
 
-  const url = new URL(request.url);
-
-  if (url.pathname === '/forum/_debug') {
-    return new Response(JSON.stringify({ origin: origin ?? null, path: url.pathname }), {
-      headers: { 'content-type': 'application/json' },
-    });
-  }
-
   if (!origin) {
     return new Response('VPS_DISCOURSE_ORIGIN not configured', { status: 503 });
   }
 
+  const url = new URL(request.url);
   const targetUrl = new URL(url.pathname + url.search, origin);
   const host = new URL(request.url).hostname;
 
@@ -27,17 +20,6 @@ export async function onRequest(
   headers.set('X-Forwarded-For', request.headers.get('CF-Connecting-IP') ?? '');
   headers.set('X-Forwarded-Proto', 'https');
   headers.set('Host', host);
-
-  if (url.pathname === '/forum/_debug2') {
-    const resp = await fetch(new Request(targetUrl.toString(), { method: 'GET', headers, redirect: 'manual' }));
-    return new Response(JSON.stringify({
-      targetUrl: targetUrl.toString(),
-      host,
-      vpsStatus: resp.status,
-      vpsContentType: resp.headers.get('content-type'),
-      vpsLocation: resp.headers.get('location'),
-    }), { headers: { 'content-type': 'application/json' } });
-  }
 
   const proxied = new Request(targetUrl.toString(), {
     method: request.method,
